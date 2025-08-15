6a86881745ed5e415fd66e4d78050b16fcb5c7d8
guzio <star.master.123.mg@gmail.com>
2025-08-15 04:30:52 +0200

    Beutified the whole „utwórz” and handled an extra error in it.

--- src/commands/utwórz.ts
+++ src/commands/utwórz.ts
@@ -5,7 +5,7 @@ import {
     SlashCommandBuilder
 } from "discord.js";
 
-import { logErrorMsg } from "functions/logger.js";
+import { logError, logErrorMsg, logInfo } from "functions/logger.js";
 import Bot from "../classes/Bot.js";
 import Strings from "../strings.json" with { type: "json" };
 
@@ -34,19 +34,26 @@ export async function run(client: Bot, interaction: ChatInputCommandInteraction)
         const users = client.db.users;
         const user  = interaction.user.id;
         const nick  = interaction.options.getString("nick", true);
-        const uuid  = interaction.options.getString("uuid", false) || (await (await fetch("https://playerdb.co/api/player/minecraft/"+nick /*Let's pray that Discord does some basic sanitisation, like removing BCPSs, BELLs, NULLs, and anything else that doesn't belong in a URL (but shouldn't belong in a user-enterable command, either).*/)).json() as {data: {player: {id: string}}}).data.player.id;
+        const uuid  = interaction.options.getString("uuid", false) || await loadMojangUUID(nick);
         
-        if (await users.findUnique({ where: { discordsnowflakeid: user } })){
-            await interaction.editReply({
-                content: `Już zarejestrowano!`
-            });
+        if (uuid === "WRONG!"){
+            logError(Strings.create_log_invalid);
+            await interaction.editReply({ content: Strings.create_invalid });
             return;
         }
         
-        if (await users.findUnique({ where: { mcname: nick } }) || await users.findUnique({ where: { mcuuid: uuid } })){
-            await interaction.editReply({
-                content: `Konto MC w użyciu!`,
-            });
+        const errors: string[] = [];
+        if(await users.findUnique({ where: { discordsnowflakeid: user } })) errors.push(Strings.create_known_discord_part1+user+Strings.create_known_discord_part2);
+        
+        const by_nick = await users.findUnique({ where: { mcname: nick } });
+        if(by_nick) errors.push(Strings.create_known_mcname_part1+nick+Strings.create_known_mcname_part2+by_nick.discordsnowflakeid+Strings.create_known_mcname_part3);
+        
+        const by_uuid = await users.findUnique({ where: { mcuuid: uuid } });
+        if(by_uuid) errors.push(Strings.create_known_mcuuid_part1+uuid+Strings.create_known_mcuuid_part2+by_uuid.discordsnowflakeid+Strings.create_known_mcuuid_part3);
+        
+        if(errors.length !== 0){
+            logError(Strings.create_log_known, ...errors);
+            await interaction.editReply({ content: [Strings.create_known_header, ...errors].join(Strings.create_known_combiner) });
             return;
         }
         
@@ -62,13 +69,27 @@ export async function run(client: Bot, interaction: ChatInputCommandInteraction)
             mcuuid: uuid
         }});
         
+        logInfo(Strings.create_log_success, user, nick, uuid);
+        
         await interaction.editReply({ content: (Strings.create_success_part1 + user + Strings.create_success_part2 + nick + Strings.create_success_part3 + uuid + Strings.create_success_part4) });
     }
     
     catch (e) {
-        logErrorMsg(e, "err")
+        logErrorMsg(e, Strings.create_log_error)
         await interaction.editReply({
-            content: `BŁĄD!`,
+            content: Strings.create_error_public,
         });
+    }
+}
+
+async function loadMojangUUID(nick: string): Promise<string>{
+    try{
+        return (await (
+            await fetch("https://playerdb.co/api/player/minecraft/"+nick
+            /*Let's pray that Discord does some basic sanitisation, like removing BCPSs, BELLs, NULLs, and anything else that doesn't belong in a URL (but shouldn't belong in a user-enterable command, either).*/)).json(
+        ) as {data: {player: {id: string}}}).data.player.id;
+    }
+    catch{
+        return "WRONG!" 
     }
 }
