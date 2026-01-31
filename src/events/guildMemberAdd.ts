bdfd713d5c5c4c47502a43c0027eab0e98f99f0f
Guzio <guzekk@protonmail.com>
2026-01-31 19:45:19 +0000

    joinEvent experiments

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -2,5 +2,6 @@ import Bot from "classes/Bot.js";
 import { GuildMember } from "discord.js";
 
 export async function run(client: Bot, member: GuildMember) {
-    // Do stuff
+    console.log(`New member joined: ${member.user.tag} (${member.id})`);
+	member.roles.add("1467236642208219218");
 }
